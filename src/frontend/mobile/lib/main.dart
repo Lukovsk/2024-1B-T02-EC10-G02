bd56029104a02b5e688fddc3148a571aee2aca9d
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2024-05-22 10:45:51 -0300

    start of admin and allow web

--- src/frontend/mobile/lib/main.dart
+++ src/frontend/mobile/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'widgets/login_page.dart';
+import 'screens/admin/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Tela de Login',
-      home: LoginPage(),
+      home: AdminHome(),
     );
   }
 }
