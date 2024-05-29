0c0507eb85154675135f68551fbb4969bc5d3af2
Jean Rothstein <jean.machado@sou.inteli.edu.br>
2024-05-29 16:55:28 -0300

    inicio serviço publicação pedido na fila

---
+++ src/backend/order_pub_service/main.py
@@ -0,0 +1,38 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from prismaClient import prismaClient
+from routes import order_pub_router
+import os
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await prismaClient.connect()
+    yield
+    await prismaClient.disconnect()
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+origins = [
+    "http://localhost:3001",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(order_pub_router)
+
+if __name__ == "__main__":
+    import uvicorn
+    import os
+
+    #if "RABBITMQ_HOST" in os.environ:
+
+    uvicorn.run(app, host="0.0.0.0", port=3001)
